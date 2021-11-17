fd909b8a5b2d5402d1a445590ca21ed44ef589ea
Siaros599 <Siaros599@users.noreply.github.com>
2021-11-17 20:03:13 +0700

    commit5

--- lib/listnews.dart
+++ lib/listnews.dart
@@ -30,6 +30,16 @@ class ListNews extends StatefulWidget {
 
 class _ListNewsState extends State<ListNews> {
   var words=generateWordPairs().take(20);
+  var image=[
+    "https://imgproxy.k7.tinhte.vn/nvkNY5ZzYUOJD0nigWttUNGDX4LjBOuEVevSmlw9qkw/h:232/plain/https://photo2.tinhte.vn/data/attachment-files/2020/09/5151463_og.jpg",
+    "https://imgproxy.k7.tinhte.vn/ZTBNYuwEoiWvy3eb_Qe_4DuPkw_9QuaseRcab0kKcb8/h:232/plain/https://photo2.tinhte.vn/data/attachment-files/2021/08/5577318_og.jpg",
+    "https://imgproxy.k7.tinhte.vn/NhBDE9RTDdIv5hpJpfSTkehHlOi7CJIay4DMhXqzpD4/h:232/plain/https://s3.cloud.cmctelecom.vn/tinhte1/2018/10/4455874_og.jpg",
+    "https://imgproxy.k7.tinhte.vn/NuY9dVtcoVAtZv1Y4L1vTybIfOAnXVBlqA85BVonFcM/h:232/plain/https://s3.cloud.cmctelecom.vn/tinhte2/2019/09/4778103_og.jpg",
+    "https://imgproxy.k7.tinhte.vn/obB-j_U4vH-9eOc49y9PriucPDsKzvZ3-FaD2TvdDRM/h:232/plain/https://photo2.tinhte.vn/data/attachment-files/2021/10/5669644_og.jpg",
+    "https://imgproxy.k7.tinhte.vn/gSN6MTTYjb3gIps9dY5q1xMAYZOe8-H-NfXJBPQKOK4/h:232/plain/https://s3.cloud.cmctelecom.vn/tinhte1/2018/10/4454564_og.jpg",
+    "https://imgproxy.k7.tinhte.vn/DhEF9bkA1mxb-t2jBKjRWHkjprR8Cl77-1prGH_iwu0/h:232/plain/https://s3.cloud.cmctelecom.vn/tinhte2/2019/03/4582338_og.jpg",
+    "https://imgproxy.k7.tinhte.vn/iZElKwsyTKhHohGB47C6YyOoi_irVJG2ZWZtJQMAsos/h:232/plain/https://s3.cloud.cmctelecom.vn/tinhte2/2019/02/4569506_og.jpg"
+    ];
    late Future<News> ns ;
    Future<News> fetchData() async {
      String url = "https://newsapi.org/v2/everything?domains=tinhte.vn&apiKey=e7f944b2666a493a807df7cb39ebee92";
@@ -66,20 +76,25 @@ class _ListNewsState extends State<ListNews> {
               child: SizedBox(
                 width: 1000,
                 child: CarouselSlider(
-                  items: words.map((e) =>  ClipRRect(
+                  items: image.map((e) =>  ClipRRect(
                     borderRadius: BorderRadius.circular(0),
                     child: Stack(
                     children: [
+                      //SizedBox(
+                        //width: 1000,
+                        //height: 90,
+                        //child: OutlinedButton(
+                            //onPressed: (){},
+                            //style: ButtonStyle(
+                              //shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))),
+                            //),
+                            //child: Text(e.asString.toUpperCase(),style: TextStyle(fontSize: 25),)
+                          //),
+                      //),
                       SizedBox(
-                        width: 1000,
-                        height: 90,
-                        child: OutlinedButton(
-                            onPressed: (){},
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))),
-                            ),
-                            child: Text(e.asString.toUpperCase(),style: TextStyle(fontSize: 25),)),
-                      ),
+                        
+                        child: Image.network(e)
+                      )
                     ],
                   ),
                   )).toList(),
